e2195e87a05f4b83fb5488bddc05a7a5ad98a1e7
Cinthya <cinthyadasilvasoares@outlook.com>
2022-11-26 05:30:34 -0300

    fazendo a promise

--- doguito_cliente/service/Cliente-Service.js
+++ doguito_cliente/service/Cliente-Service.js
@@ -16,15 +16,27 @@ const criaNovaLinha = (nome, email) => {
 
 const tabela = document.querySelector('[data-tabela')
 
-const http = new XMLHttpRequest();
-http.open('GET', 'http://localhost:3000/profile');
+const listaClientes = () =>  {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+            http.open('GET', 'http://localhost:3000/profile');
 
-http.send();
-
-http.onload = () => {
-    const data = JSON.parse(http.response)
-    data,array.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-    });
-
+            http.onload = () => {
+                if(http.status >= 400){
+                    reject(JSON.parse(http.response))
+                } else{
+                    resolve(JSON.parse(http.response))
+                }
+            }
+            http.send();
+        });    
+        console.log(promise);
+    return promise; 
 }
+listaClientes();
+.then( data => {
+    data.forEach(elemento => {
+    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+    })
+})
+
