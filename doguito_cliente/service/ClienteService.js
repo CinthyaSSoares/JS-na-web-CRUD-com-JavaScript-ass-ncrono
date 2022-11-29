15dca642839b65994fcbbfa8731ff88b60a58367
Cinthya <cinthyadasilvasoares@outlook.com>
2022-11-29 11:00:02 -0300

    Edição de cliente concluida

--- doguito_cliente/service/ClienteService.js
+++ doguito_cliente/service/ClienteService.js
@@ -5,6 +5,8 @@ const listaClientes = () =>  {
     })
 }
 
+
+// Criando cliente
 const criaCliente = (nome, email) => {
     return fetch(`http://localhost:3000/profile`, {
         method: 'POST',
@@ -21,16 +23,43 @@ const criaCliente = (nome, email) => {
     })
 }
 
+// Conexão para remover cliente
 const removeCliente = (id) => {
     return fetch(`http://localhost:3000/profile/${id}`,{
         method: 'DELETE'
     })
 }
 
+// Conexão para editar cliente
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then( resposta => {
+        return resposta.json()
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then(resposta =>{
+        return resposta.json()
+    })
+}
+
+// clienteService antes das funções relacionadas para chamar
 export const clienteService = {
     listaClientes,
     criaCliente,
-    removeCliente
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
 
-// clienteService antes das funções relacionadas
